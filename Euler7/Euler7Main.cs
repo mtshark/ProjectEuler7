c3d64924cf1fbe0eadb57375d5caec7b20a5e811
mtshark <mtschark@yahoo.com>
2014-06-27 08:03:09 -0400

    Provide a UI

--- Euler7/Euler7Main.cs
+++ Euler7/Euler7Main.cs
@@ -5,7 +5,13 @@ using System.Text;
 
 namespace Euler7 {
     public class Euler7Main {
+        private const int PrimeRequiredForSolution = 10001;
         static void Main(string[] args) {
+            List<long> primes = GetPrimes(PrimeRequiredForSolution);
+            for (int i = 0; i < primes.Count; i++) {
+                Console.WriteLine("prime number " + i + " is: " + primes[i]);
+            }
+            Console.ReadKey();
         }
 
         public static bool IsPrime(int candidate) {
@@ -21,12 +27,12 @@ namespace Euler7 {
             return candidate != 1;
         }
 
-        public static List<long> GetPrimes(int numberOfPrimesToFind){
-        List<long> primes = new List<long>();
+        public static List<long> GetPrimes(int numberOfPrimesToFind) {
+            List<long> primes = new List<long>();
             if (numberOfPrimesToFind < 1)
                 return primes;
             primes.Add(1);
-            if (numberOfPrimesToFind>1)
+            if (numberOfPrimesToFind > 1)
                 primes.Add(2);
             for (int i = 3; primes.Count <= numberOfPrimesToFind; i += 2) {
                 if (IsPrime(i))
