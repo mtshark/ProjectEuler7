4e1a79b8ff1c812f90be85d8c72b56fef1bd53e8
mtshark <mtschark@yahoo.com>
2014-06-27 07:56:00 -0400

    ProjectEuler7 1001st prime number

---
+++ Euler7/Euler7Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler7 {
+    public class Euler7Main {
+        static void Main(string[] args) {
+        }
+
+        public static bool IsPrime(int candidate) {
+            if ((candidate & 1) == 0) {
+                return candidate == 2;
+            }
+
+            for (int i = 3; (i * i) <= candidate; i += 2) {
+                if ((candidate % i) == 0) {
+                    return false;
+                }
+            }
+            return candidate != 1;
+        }
+
+        public static List<long> GetPrimes(int numberOfPrimesToFind){
+        List<long> primes = new List<long>();
+            if (numberOfPrimesToFind < 1)
+                return primes;
+            primes.Add(1);
+            if (numberOfPrimesToFind>1)
+                primes.Add(2);
+            for (int i = 3; primes.Count <= numberOfPrimesToFind; i += 2) {
+                if (IsPrime(i))
+                    primes.Add(i);
+            }
+            return primes;
+        }
+    }
+}
---
+++ TestEuler7/TestEuler7.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Euler7;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestEuler7 {
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void IsPrimeTrue() {
+            bool ACTUAL = Euler7Main.IsPrime(13);
+            Assert.IsTrue(ACTUAL);
+        }
+
+        [TestMethod]
+        public void IsPrimeFalse() {
+            bool ACTUAL = Euler7Main.IsPrime(9);
+            Assert.IsFalse(ACTUAL);
+        }
+
+        [TestMethod]
+        public void GetAllPrimesLowAmount() {
+            int EXPECTED = 23;
+            List<long> ACTUAL = Euler7Main.GetPrimes(10);
+            Assert.AreEqual(EXPECTED, ACTUAL[9]);
+        }
+
+        [TestMethod]
+        public void GetAllPrimesEulerExample() {
+            int EXPECTED = 13;
+            List<long> ACTUAL = Euler7Main.GetPrimes(6);
+            Assert.AreEqual(EXPECTED, ACTUAL.Last());
+        }
+
+        [TestMethod]
+        public void GetAllPrimesActualExcersize() {
+            int EXPECTED = 104743;
+            List<long> ACTUAL = Euler7Main.GetPrimes(10001);
+            Assert.AreEqual(EXPECTED, ACTUAL.Last());
+        }
+
+        [TestMethod]
+        public void GetAllPrimesVeryHigh() {
+            int EXPECTED = 4999999;
+            List<long> ACTUAL = Euler7Main.GetPrimes(348513);
+            Assert.AreEqual(EXPECTED, ACTUAL.Last());
+        }
+    }
+}
